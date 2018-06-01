257e725bf6910051738cb8a1fa149c8ceefbf95e
LHui Liu <yogyliu@163.com>
2018-06-01 18:22:06 +0800

    third commit
    revise some error,but the cluster center initialization method may be  not the best choice

--- island_loss.py
+++ island_loss.py
@@ -2,13 +2,12 @@ import tensorflow as tf
 import itertools
 import numpy as np
 
-def get_center_loss(features, labels, alpha,alpha1, num_classes):
+def get_lc_center_loss(features, labels, alpha,alpha1, num_classes):
 
     len_features = features.get_shape()[1]
 
     centers = tf.get_variable('centers', [num_classes, len_features], dtype=tf.float32,
-                              initializer=tf.constant_initializer(0), trainable=False)
-
+                              initializer=tf.random_normal_initializer(), trainable=False)
     labels = tf.reshape(labels, [-1])
 
     centers_batch = tf.gather(centers, labels)
@@ -20,9 +19,10 @@ def get_center_loss(features, labels, alpha,alpha1, num_classes):
     combination = itertools.permutations(index,2)
 
     pairwise_grad_val = {}
+    pair_distance_loss = []
     for idx,item in enumerate(combination):
         index = idx/(num_classes-1)
-        lc_grad = pairwise_grad(features,item)
+        lc_grad = pairwise_grad(centers_batch,item)
         if idx%(num_classes-1) == 0:
             if index in pairwise_grad_val:
                 pairwise_grad_val[index] += lc_grad
@@ -33,6 +33,7 @@ def get_center_loss(features, labels, alpha,alpha1, num_classes):
                 pairwise_grad_val[index] += lc_grad
             else:
                 pairwise_grad_val[index] = lc_grad
+        pair_distance_loss.append(pairwise_distance(centers_batch, item))
 
     grad_pairwise = []
     for idx in range(num_classes):
@@ -40,14 +41,14 @@ def get_center_loss(features, labels, alpha,alpha1, num_classes):
 
     grad_pairwise = tf.convert_to_tensor(grad_pairwise)
 
-    grad_pairwsise_batch = tf.gather(grad_pairwise,labels)
+    grad_pairwise_batch = tf.gather(grad_pairwise,labels)
 
-    pair_distance_loss = []
-    pair_distance_loss.append(map(lambda x:pairwise_distance(centers_batch,x),combination))
+    #pair_distance_loss = []
+    #pair_distance_loss.append(map(lambda x:pairwise_distance(centers_batch,x),combination))
 
     pair_distance_loss = tf.reduce_sum(pair_distance_loss)
 
-    loss = loss_part1 + alpha1*tf.reduce_sum(pair_distance_loss)
+    loss = loss_part1 + alpha1*pair_distance_loss
 
     ####new code end
 
@@ -58,12 +59,12 @@ def get_center_loss(features, labels, alpha,alpha1, num_classes):
     appear_times = tf.reshape(appear_times, [-1, 1])
 
     diff = diff / tf.cast((1 + appear_times), tf.float32)
-    diff = diff + alpha1*grad_pairwsise_batch/(num_classes-1)
+    diff = diff + alpha1*grad_pairwise_batch/(num_classes-1)
 
     diff = alpha * diff
     centers_update_op = tf.scatter_sub(centers, labels, diff)
 
-    return loss, centers, centers_update_op
+    return loss, centers, centers_update_op,loss_part1,pair_distance_loss
 
 
 
@@ -75,11 +76,11 @@ def pairwise_distance(fea,com):
     else:
         print ("Please check the feature dimensions")
         return
-    k_l2norm = tf.nn.l2_normalize(fea_k)
-    j_l2norm = tf.nn.l2_normalize(fea_j)
-    loss_term = k_l2norm*j_l2norm + 1
-    grad_term = k_l2norm/tf.norm(fea_j,ord=2) - k_l2norm*tf.square(j_l2norm)/tf.norm(fea_j,ord=2)
-    return loss_term,grad_term
+    k_l2norm = tf.nn.l2_normalize(fea_k,0)
+    j_l2norm = tf.nn.l2_normalize(fea_j,0)
+    loss_term = tf.reduce_sum(tf.multiply(k_l2norm,j_l2norm)) + 1
+    #grad_term = k_l2norm/tf.norm(fea_j,ord=2) - k_l2norm*tf.square(j_l2norm)/tf.norm(fea_j,ord=2)
+    return loss_term
 
 def pairwise_grad(fea,com):
     dims = len(fea.get_shape())
@@ -106,4 +107,5 @@ if __name__ == "__main__":
     alpha   = 0.4
     alpha2  = 0.5
     num_classes = 7
-    get_center_loss(feature,label,alpha,alpha2,num_classes)
+    get_lc_center_loss(feature,label,alpha,alpha2,num_classes)
+    print("hello")
