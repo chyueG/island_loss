c604b00004476c3a89ea3f657f3a16452c83b38e
LHui Liu <yogyliu@163.com>
2018-06-21 10:11:47 +0800

    Update island_loss.py

--- island_loss.py
+++ island_loss.py
@@ -7,7 +7,7 @@ def get_lc_center_loss(features, labels, alpha,alpha1, num_classes):
     len_features = features.get_shape()[1]
 
     centers = tf.get_variable('centers', [num_classes, len_features], dtype=tf.float32,
-                              initializer=tf.random_normal_initializer(), trainable=False)
+                              initializer=tf.contrib.layers.xavier_initializer(), trainable=False)
     labels = tf.reshape(labels, [-1])
 
     centers_batch = tf.gather(centers, labels)
@@ -43,6 +43,7 @@ def get_lc_center_loss(features, labels, alpha,alpha1, num_classes):
 
     grad_pairwise_batch = tf.gather(grad_pairwise,labels)
 
+    grad_pairwise_batch = tf.squeeze(grad_pairwise_batch,axis=1)
     #pair_distance_loss = []
     #pair_distance_loss.append(map(lambda x:pairwise_distance(centers_batch,x),combination))
 
@@ -60,8 +61,11 @@ def get_lc_center_loss(features, labels, alpha,alpha1, num_classes):
 
     diff = diff / tf.cast((1 + appear_times), tf.float32)
     diff = diff + alpha1*grad_pairwise_batch/(num_classes-1)
-
     diff = alpha * diff
+
+    #diff = alpha*diff + alpha1*grad_pairwise_batch/(num_classes-1)
+
+
     centers_update_op = tf.scatter_sub(centers, labels, diff)
 
     return loss, centers, centers_update_op,loss_part1,pair_distance_loss
@@ -76,8 +80,10 @@ def pairwise_distance(fea,com):
     else:
         print ("Please check the feature dimensions")
         return
-    k_l2norm = tf.nn.l2_normalize(fea_k,0)
-    j_l2norm = tf.nn.l2_normalize(fea_j,0)
+    fea_k = tf.expand_dims(fea_k,0)
+    fea_j = tf.expand_dims(fea_j,0)
+    k_l2norm = tf.nn.l2_normalize(fea_k,1)###pay attention to aixs
+    j_l2norm = tf.nn.l2_normalize(fea_j,1)
     loss_term = tf.reduce_sum(tf.multiply(k_l2norm,j_l2norm)) + 1
     #grad_term = k_l2norm/tf.norm(fea_j,ord=2) - k_l2norm*tf.square(j_l2norm)/tf.norm(fea_j,ord=2)
     return loss_term
@@ -90,11 +96,12 @@ def pairwise_grad(fea,com):
     else:
         print ("Please check the feature dimensions")
         return
+    fea_k = tf.expand_dims(fea_k,0)
+    fea_j = tf.expand_dims(fea_j,0)
     k_l2norm = tf.nn.l2_normalize(fea_k)
     j_l2norm = tf.nn.l2_normalize(fea_j)
     grad_term = k_l2norm/tf.norm(fea_j,ord=2) - k_l2norm*tf.square(j_l2norm)/tf.norm(fea_j,ord=2)
     return grad_term
-
 
 if __name__ == "__main__":
     feature = np.arange(8192).reshape((32,256))
